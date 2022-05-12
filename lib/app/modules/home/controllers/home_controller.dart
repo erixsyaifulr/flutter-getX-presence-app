8d0e223fec238fa646c1caddcc4757a88f727ee1
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-05-12 16:29:28 +0700

    add stream function to get today presence

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../../../routes/app_pages.dart';
 
@@ -26,6 +27,18 @@ class HomeController extends GetxController {
         .collection("presences")
         .orderBy("date")
         .limitToLast(5)
+        .snapshots();
+  }
+
+  Stream<DocumentSnapshot<Map<String, dynamic>>> streamTodayPresence() async* {
+    String uid = auth.currentUser!.uid;
+    String todayId =
+        DateFormat.yMd().format(DateTime.now()).replaceAll("/", "-");
+    yield* firestore
+        .collection("users")
+        .doc(uid)
+        .collection("presences")
+        .doc(todayId)
         .snapshots();
   }
 }
